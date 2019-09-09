8c04f22c5df46fcd780d6427eba69dfbb0631f2f
gajdosech2 <l.gajdosech@gmail.com>
2019-09-09 21:20:40 +0200

    labels loading

--- Model/BasicModel.py
+++ Model/BasicModel.py
@@ -11,21 +11,35 @@ feature_count = 5
 input_count = tile_size * tile_size * feature_count
 
 
-def load_train_files(folder_name: str) -> np.array:
+def load_train_data(dataset_name: str) -> np.array:
     """
     Load data files from the given folder.
 
-    :param folder_name: Name of the folder followed by '/' containing the data files
+    :param dataset_name: Name of the data set.
     :return: np.array of data files in (input_count, ) shape
     """
-    files_folder = "NeuralNetworkFiles/"
-    data_files = os.listdir(files_folder + folder_name)
-    data_array = []
-    for file in data_files:
-        path = files_folder + folder_name + file
-        data = np.loadtxt(path, skiprows=1, delimiter=',')
-        data_array.append(data.flatten())
+    data_path = "NeuralNetworkFiles/" + dataset_name + "_data/"
+    data_files = os.listdir(data_path)
+    data_array = [None] * len(data_files)
+    for i, file in enumerate(data_files):
+        data = np.loadtxt(data_path + file, skiprows=1, delimiter=',')
+        data_array[i] = data.flatten()
     return np.array(data_array)
+
+
+def load_train_labels(dataset_name: str) -> np.array:
+    """
+    Load data labels.
+
+    :param dataset_name: Name of the data set.
+    :return: np.array of data files in (input_count, ) shape
+    """
+    labels_file = "NeuralNetworkFiles/" + dataset_name + "_truth.csv"
+    labels = np.loadtxt(labels_file, delimiter=',')
+    labels_array = [None] * len(labels)
+    for i, label in enumerate(labels):
+        labels_array[i] = label[1]
+    return np.array(labels_array)
 
 
 def build_model() -> keras.Sequential:
@@ -35,7 +49,8 @@ def build_model() -> keras.Sequential:
     :return: Neural network model as keras.Sequential object.
     """
     network_model = keras.Sequential([
-        keras.layers.Dense(128, activation=tf.nn.relu, input_shape=(input_count,)),
+        keras.layers.Dense(512, activation=tf.keras.activations.sigmoid, input_shape=(input_count,)),
+        keras.layers.Dense(256, activation=tf.keras.activations.relu),
         keras.layers.Dense(1, activation=tf.keras.activations.linear)
     ])
     network_model.compile(optimizer='adam',
@@ -44,7 +59,13 @@ def build_model() -> keras.Sequential:
     return network_model
 
 
+dataset = "small"
+train_data = load_train_data(dataset)
+train_labels = load_train_labels(dataset)
+print(train_data)
+print(train_labels)
 model = build_model()
-train_data = load_train_files("apple_01_data/")
+model.fit(train_data, train_labels, epochs=20)
+
 prediction = model.predict(train_data)
 print(prediction)
