7b1bacb0f7ea3718f77ed840772f3e93866f01b6
gajdosech2 <l.gajdosech@gmail.com>
2020-07-11 12:17:17 +0200

    rename linux cc

--- ClionCC/COGSConverter.cpp
+++ ClionCC/COGSConverter.cpp
@@ -1,9 +1,14 @@
 #include "DataFormatter.h"
+#include <cstring>
 
 int main(int argc, char* argv[])
 {
     DataFormatter formatter;
     formatter.Import(argv[1]);
+    if (argc > 2 && strcmp("--trim", argv[2]) == 0)
+    {
+        formatter.Trim();
+    }
     formatter.GenerateImageFiles();
     return 0;
 }
